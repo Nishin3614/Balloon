6f53009e6280b4cef0de48380eed6ac12eb62df4
masayasu wakita <koamchi.w315@gmail.com>
2020-07-09 15:34:16 +0900

    サーバーとの交信頻度を60FPSに固定しました。

--- cpp&h/network.cpp
+++ cpp&h/network.cpp
@@ -74,57 +74,76 @@ void CNetwork::Uninit(void)
 // ------------------------------------------
 void CNetwork::Update(void)
 {
+	DWORD dwCurrentTime;		// 現在時刻
+	DWORD dwExecLastTime;		// 最後に処理した時刻
+	DWORD dwFrameCount;
+	DWORD dwFPSLastTime;
+
+	dwCurrentTime = 0;
+	dwExecLastTime = timeGetTime();		// システム時刻をミリ秒単位で取得
+
+	dwFrameCount = 0;
+	dwFPSLastTime = timeGetTime();		// システム時刻をミリ秒単位で取得
+
 	while (m_bGame)
 	{
-		char debug[1024];
-		int nError = -1;
-		float fData[NUM_KEY_M];
-		char cDie[32];
+		// DirectXの処理
+		dwCurrentTime = timeGetTime();		// システム時刻を取得
 
-		char cDataText[128];		//文字
-		char cPlayerData[MAX_PLAYER][128];		//比較
-
-		OutputDebugString("送信開始\n");
-		KeyData();
-		OutputDebugString("送信完了\n");
-
-		OutputDebugString("受信開始\n");
-		nError = recv(m_sockServerToClient, debug, sizeof(debug), 0);
-		OutputDebugString("受信完了\n");
-
-		if (nError == SOCKET_ERROR)
+		if ((dwCurrentTime - dwExecLastTime) >= (1000 / 60))		// 1/60秒ごとに実行
 		{
-			// ソケットの作成に失敗したとき
-			char aError[64];
-			sprintf(aError, "サーバーに接続失敗!!\n エラーコード : %d", WSAGetLastError());
-			MessageBox(NULL, aError, "警告！", MB_ICONWARNING);
+			dwExecLastTime = dwCurrentTime;	// 処理した時刻を保存
 
-		}
-		else
-		{
-			sscanf(debug, "%s %s %s %s %s", &cDie, &cPlayerData[0], &cPlayerData[1], &cPlayerData[2], &cPlayerData[3]);
+			char debug[1024];
+			int nError = -1;
+			float fData[NUM_KEY_M];
+			char cDie[32];
 
-			for (int nCount = 0; nCount < MAX_PLAYER; nCount++)
+			char cDataText[128];		//文字
+			char cPlayerData[MAX_PLAYER][128];		//比較
+
+			OutputDebugString("送信開始\n");
+			KeyData();
+			OutputDebugString("送信完了\n");
+
+			OutputDebugString("受信開始\n");
+			nError = recv(m_sockServerToClient, debug, sizeof(debug), 0);
+			OutputDebugString("受信完了\n");
+
+			if (nError == SOCKET_ERROR)
 			{
-				ConvertStringToFloat(cPlayerData[nCount], ",", fData);
+				// ソケットの作成に失敗したとき
+				char aError[64];
+				sprintf(aError, "サーバーに接続失敗!!\n エラーコード : %d", WSAGetLastError());
+				MessageBox(NULL, aError, "警告！", MB_ICONWARNING);
 
-				for (int nCntkey = 0; nCntkey < NUM_KEY_M; nCntkey++)
-				{
-					m_aKeyStateOld[nCount][nCntkey] = m_aKeyState[nCount][nCntkey];
-				}
-
-				for (int nCntKey = 0; nCntKey < NUM_KEY_M; nCntKey++)
-				{
-					m_aKeyState[nCount][nCntKey] = fData[nCntKey];
-				}
-
-				for (int nCntKey = 0; nCntKey < NUM_KEY_M; nCntKey++)
-				{
-					m_aKeyStateTrigger[nCount][nCntKey] = (m_aKeyStateOld[nCount][nCntKey] ^ m_aKeyState[nCount][nCntKey]) & m_aKeyState[nCount][nCntKey];
-				}
 			}
+			else
+			{
+				sscanf(debug, "%s %s %s %s %s", &cDie, &cPlayerData[0], &cPlayerData[1], &cPlayerData[2], &cPlayerData[3]);
 
-			CDebugproc::Print("キー入力情報 : %d , %d , %d , %d , %d", m_aKeyState[m_nId][NUM_KEY_A], m_aKeyState[m_nId][NUM_KEY_S], m_aKeyState[m_nId][NUM_KEY_W], m_aKeyState[m_nId][NUM_KEY_D], m_aKeyState[m_nId][NUM_KEY_SPACE]);
+				for (int nCount = 0; nCount < MAX_PLAYER; nCount++)
+				{
+					ConvertStringToFloat(cPlayerData[nCount], ",", fData);
+
+					for (int nCntkey = 0; nCntkey < NUM_KEY_M; nCntkey++)
+					{
+						m_aKeyStateOld[nCount][nCntkey] = m_aKeyState[nCount][nCntkey];
+					}
+
+					for (int nCntKey = 0; nCntKey < NUM_KEY_M; nCntKey++)
+					{
+						m_aKeyState[nCount][nCntKey] = fData[nCntKey];
+					}
+
+					for (int nCntKey = 0; nCntKey < NUM_KEY_M; nCntKey++)
+					{
+						m_aKeyStateTrigger[nCount][nCntKey] = (m_aKeyStateOld[nCount][nCntKey] ^ m_aKeyState[nCount][nCntKey]) & m_aKeyState[nCount][nCntKey];
+					}
+				}
+
+				CDebugproc::Print("キー入力情報 : %d , %d , %d , %d , %d", m_aKeyState[m_nId][NUM_KEY_A], m_aKeyState[m_nId][NUM_KEY_S], m_aKeyState[m_nId][NUM_KEY_W], m_aKeyState[m_nId][NUM_KEY_D], m_aKeyState[m_nId][NUM_KEY_SPACE]);
+			}
 		}
 	}
 }
